9e49ccee2a8d253b267750ac72881129eaadd57f
группы стационара <st@itstep.lan>
2023-12-17 19:38:21 +0300

    start work on deleting cards

--- registration/scripst/submit.js
+++ registration/scripst/submit.js
@@ -8,7 +8,6 @@ function sendForm(event){
         addressField.innerHTML = error.address;
         addressField.previousElementSibling.classList.add("error");
     }else{
-        error.address = null;
         let addressField = document.getElementById("address-error");
         addressField.innerHTML = '';
         addressField.previousElementSibling.classList.remove("error");
@@ -22,7 +21,6 @@ function sendForm(event){
         nameField.innerHTML = error.name;
         nameField.previousElementSibling.classList.add("error");
     }else{
-        error.name = null;
         let nameField = document.getElementById("name-error");
         nameField.innerHTML = '';
         nameField.previousElementSibling.classList.remove("error");
@@ -37,11 +35,62 @@ function sendForm(event){
         phoneField.innerHTML = error.phone;
         phoneField.previousElementSibling.classList.add("error");
     }else{
-        error.phone = null;
         let phoneField = document.getElementById("phone-error");
         phoneField.innerHTML = '';
         phoneField.previousElementSibling.classList.remove("error");
     }
 
+    let date = event.target[1].value;
+    let gender = event.target[2].value;
+    let email = event.target[3].value;
+    let url = event.target[4].value;
+    let errors = 0;
+
+    for(key in error){
+        errors++;
+    }
+
+    if(errors){
+        return false;
+    }else{
+        let card = document.getElementsByClassName("card-list")[0];
+
+        let now = new Date();
+        let birthday = new Date(date);
+        let age = now.getFullYear() - birthday.getFullYear();
+
+        card.insertAdjacentHTML(
+            "beforeend",
+            `
+            <div class="card" draggable="true" ondragstart="startDrag(event) id = "user-${now-getMilliseconds()}">
+                <img src="images/avatar.png" alt="">
+                <div class="card-name">${name}</div>
+                <div class="card-age">Возраст: ${age}</div>
+                <button>Инфо</button>
+            </div>`);
+    }
+
     return false;
+}
+
+function startDrag(event){
+    event.dataTransfer.setData("text/html", event.target.id);
+    event.dataTransfer.effectAllowed = "move";
+}
+
+function enterDrag(event){
+    event.dataTransfer.dropEffect = "move";
+    event.target.classList.add("active");
+}
+
+function leaveDrag(event){
+    event.target.classList.remove("active");
+}
+
+function drop(event){
+    let cart = document.getElementsByClassName("card-remove")[0];
+
+    if(confirm("Вы действительно хотите удалить карточку?")){
+        let element
+    }
 }
