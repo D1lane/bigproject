62d1f750105d0af90d503f8e2dc1f1b5f2bcc375
группы стационара <st@itstep.lan>
2024-01-14 19:40:23 +0300

    localStorage work

--- registration/scripst/submit.js
+++ registration/scripst/submit.js
@@ -1,26 +1,54 @@
+if(window.localStorage.getItem("users"))
+{
+    let users = JSON.parse(window.localStorage.getItem("users"));
+    let cardList = document.getElementsByClassName("card-list")[0];
+
+    if(cardList){
+        if(users.length > 0){
+            cardList.innerHTML = "";
+
+            users.forEach(function(user){
+                let newCard = 
+                `
+                <div class="card" draggable="true" ondragstart="startDrag(event)" id="user-${user.id}">
+                    <img src="images/avatar.png" alt="">
+                    <div class="card-name">${user.name}</div>
+                    <div class="card-age">Возраст: ${user.age}</div>
+                    <button>Инфо</button>
+                `
+                cardList.insertAdjacentHTML("beforeend", newCard);
+            });
+        }
+    }
+}
+
 function sendForm(event){
+
     let error = {};
     let address = event.target[6].value;
 
     if(address.length < 10){
-        error.address = "Ваш адрес слишком короткий.";
+        error.address = "Слишком короткий адрес";
         let addressField = document.getElementById("address-error");
         addressField.innerHTML = error.address;
         addressField.previousElementSibling.classList.add("error");
-    }else{
+    }
+    else{
         let addressField = document.getElementById("address-error");
         addressField.innerHTML = '';
         addressField.previousElementSibling.classList.remove("error");
     }
+
     let name = event.target[0].value;
     let nameTemplate = /^[А-Я][а-яА-Я\s]*[а-яА-Я]$/g;
 
     if(!nameTemplate.test(name)){
         error.name = "Введите корректное имя";
-        let nameField = document.getElementById("name-error");
+        let nameField = document.getElementById("name-error")
         nameField.innerHTML = error.name;
         nameField.previousElementSibling.classList.add("error");
-    }else{
+    }
+    else{
         let nameField = document.getElementById("name-error");
         nameField.innerHTML = '';
         nameField.previousElementSibling.classList.remove("error");
@@ -30,11 +58,12 @@ function sendForm(event){
     let phoneTemplate = /^\+375[0-9]{9}$/g;
 
     if(!phoneTemplate.test(phone)){
-        error.phone = "Введите правильный номер телефона";
-        let phoneField = document.getElementById("phone-error");
+        error.phone = "Введите правильный номер";
+        let phoneField = document.getElementById("phone-error")
         phoneField.innerHTML = error.phone;
         phoneField.previousElementSibling.classList.add("error");
-    }else{
+    }
+    else{
         let phoneField = document.getElementById("phone-error");
         phoneField.innerHTML = '';
         phoneField.previousElementSibling.classList.remove("error");
@@ -52,45 +81,72 @@ function sendForm(event){
 
     if(errors){
         return false;
-    }else{
-        let card = document.getElementsByClassName("card-list")[0];
+    }
+    else{
+        let cardList = document.getElementsByClassName("card-list")[0];
+        
+       
 
         let now = new Date();
         let birthday = new Date(date);
         let age = now.getFullYear() - birthday.getFullYear();
+        let id = Math.floor(Math.random() * 1000);
 
-        card.insertAdjacentHTML(
-            "beforeend",
-            `
-            <div class="card" draggable="true" ondragstart="startDrag(event) id = "user-${now-getMilliseconds()}">
-                <img src="images/avatar.png" alt="">
-                <div class="card-name">${name}</div>
-                <div class="card-age">Возраст: ${age}</div>
-                <button>Инфо</button>
-            </div>`);
+        let newCard = cardList.insertAdjacentHTML("beforeend", 
+        `<div class="card">
+        <img src="images/avatar.png">
+        <div class="card-name">${name}</div>
+        <div class="card-age">Возраст: ${age}</div>
+        <button class="">Инфо</button>
+        </div>`);
+
+        let cards = document.getElementsByClassName("card");
+
+        if(cards.length > 0){
+            cardList.insertAdjacentHTML("beforeEnd", newCard);
+        }
+        else{
+            cardList.innerHTML = newCard;
+        }
+
+        let user = {
+            id: id,
+            name: event.target[0].value,
+            birthday: event.target[1].value,
+            age: now.getFullYear() - birthday.getFullYear(),
+            gender: event.target[2].value,
+            phone: event.target[3].value,
+            email: event.target[4].value,
+            url: event.target[5].value,
+            address: event.target[6].value
+        }
+
+        //let userData = JSON.stringify(user);
+        let users = [];
+
+        if(window.localStorage.getItem("users")){
+            users = JSON.parse(window.localStorage.getItem("users"));
+        }
+
+        users.push(user);
+
+        window.localStorage.setItem("users", JSON.stringify(users));
+
     }
-
     return false;
 }
 
-function startDrag(event){
-    event.dataTransfer.setData("text/html", event.target.id);
-    event.dataTransfer.effectAllowed = "move";
-}
-
-function enterDrag(event){
-    event.dataTransfer.dropEffect = "move";
-    event.target.classList.add("active");
-}
-
-function leaveDrag(event){
-    event.target.classList.remove("active");
-}
-
-function drop(event){
-    let cart = document.getElementsByClassName("card-remove")[0];
-
-    if(confirm("Вы действительно хотите удалить карточку?")){
-        let element
+    function startDrag(event){
+        event.dataTransfer.setData("text/html", event.target.id);
+        event.dataTransfer.effectAllowed = "move";
     }
-}
+
+    function enterDrag(event){
+        console.log(event.target);
+        event.dataTransfer.dropEffect = "move";
+        event.target.parentElement.classList.add("active");   
+    }
+
+    function leaveDrag(event){
+        event.target.parentElement.classList.remove("active");
+    }
